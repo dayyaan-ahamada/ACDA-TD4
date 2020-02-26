7f0b1581e44a3915ef999fdb5c3e97a802ee73fe
Ahamada Dayyaan <dayyaan.ahamada@etu.umontpellier.fr>
2020-02-26 12:11:29 +0100

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -1,2 +1,20 @@
 public class Triangle {
+
+    public int triangle(int a, int b, int c){
+        int result = 0;
+
+        if(a == b && b == c)
+            result = 2;
+
+        else if(a == b || b == c || a == c)
+            result = 1;
+
+        else if(a != b && a != c && b != c)
+            result = 3;
+
+        else
+            result = 4;
+
+        return result;
+    }
 }
