41ac00b6e119905e3fc06d81cafe1f0d015fb8f1
Ahamada Dayyaan <dayyaan.ahamada@etu.umontpellier.fr>
2020-02-26 11:48:13 +0100

    Matrice

--- src/App.java
+++ src/App.java
@@ -1,10 +1,19 @@
 public class App {
     public static void main(String[] args) {
-        int tab[]=new int[6];
+        /*int tab[]=new int[6];
         Square square = new Square();
         tab= square.racine(2,6);
         for (int i=0;i<tab.length;i++){
             System.out.println(tab[i]);
+        }*/
+
+        Matrice mat = new Matrice();
+        int[][] tab = mat.matrix(4, 5, 2, 6);
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab.length; j++) {
+                System.out.print(tab[i][j]);
+            }
+            System.out.println();
         }
     }
 }
--- src/Matrice.java
+++ src/Matrice.java
@@ -1,13 +1,16 @@
+import java.util.Random;
+
 public class Matrice {
 
     public int[][] matrix(int m, int n, int a, int b){
         int[][] matrice = new int[m][n];
 
-        for(int i = 0; i<matrice.length;i++)
-            for(int j = 0; j<matrice.length;j++){
-                int nombre = (int) (Math.random()*100) + a;
+        for(int i = 0; i<m;i++)
+            for(int j = 0; j<n;j++){
+                int nombre = a + (int) (Math.random() * ((b-a) + 1));
                 matrice[i][j] = nombre;
             }
         return matrice;
     }
+
 }
