daca9153473f8bf3fbe17b51ffe0b2a5857c1ffd
maxime baurens <maxime.baurens@etu.umontpellier.fr>
2020-02-26 12:22:05 +0100

    palindrome

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,2 +1,12 @@
 public class Palindrome {
+    public int palindrome(String phrase){
+        final String SEPARATEUR = " ";
+        String mots[] = phrase.split(SEPARATEUR);
+        int nb_palindrome=0;
+
+        for (int i=0;i<mots.length;i++){
+        char[] tabMot = new [15];
+        tabMot=mots[i].split("");
+        }
+    }
 }
