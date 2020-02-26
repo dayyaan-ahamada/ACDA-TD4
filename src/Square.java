4576f5397cc5d45f1279c2ee3bc358ade9681737
maxime baurens <maxime.baurens@etu.umontpellier.fr>
2020-02-26 11:04:55 +0100

    main

--- src/Square.java
+++ src/Square.java
@@ -1,10 +1,18 @@
 public class Square {
-    public int[] racine(int A,int B){
+    public static int[] racine(int A,int B){
         int tab[] = new int[1+(B-A)];
         int indice_tab=0;
         for (int i=A;i<=B;i++){
             tab[indice_tab]=(int)Math.sqrt(i);
             indice_tab++;
+        }
+        return tab;
+    }
+    public static void main(String[] args) {
+        int tab[]=new int[6];
+        tab=racine(2,6);
+        for (int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
         }
     }
 }
