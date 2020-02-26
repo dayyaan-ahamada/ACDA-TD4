16b99537ae035ebade5eba5f9c89fcec299f00dc
maxime baurens <maxime.baurens@etu.umontpellier.fr>
2020-02-26 11:12:03 +0100

    main

--- src/App.java
+++ src/App.java
@@ -1,2 +1,9 @@
 public class App {
+    public static void main(String[] args) {
+        int tab[]=new int[6];
+        tab= Square.racine(2,6);
+        for (int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
+    }
 }
