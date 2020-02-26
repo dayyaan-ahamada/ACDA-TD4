f46e315db5e3bba843e08bdbe518a6d0ade7961a
Ahamada Dayyaan <dayyaan.ahamada@etu.umontpellier.fr>
2020-02-26 12:21:22 +0100

    Triangle

--- src/Triangle.java
+++ src/Triangle.java
@@ -3,16 +3,18 @@ public class Triangle {
     public int triangle(int a, int b, int c){
         int result = 0;
 
-        if(a == b && b == c)
-            result = 2;
+        if(a > 0 && b > 0 && c > 0) {
 
-        else if(a == b || b == c || a == c)
-            result = 1;
+            if (a == b && b == c)
+                result = 2;
 
-        else if(a != b && a != c && b != c)
-            result = 3;
+            else if (a == b || b == c || a == c)
+                result = 1;
 
-        else
+            else if (a != b && b != c)
+                result = 3;
+
+        }else
             result = 4;
 
         return result;
--- test/TriangleTest.java
+++ test/TriangleTest.java
@@ -1,9 +1,15 @@
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class TriangleTest {
+
+    private int a;
+    private int b;
+    private int c;
+    private Triangle triangle = new Triangle();
 
     @BeforeEach
     void setUp() {
@@ -11,5 +17,37 @@ class TriangleTest {
 
     @AfterEach
     void tearDown() {
+        a = 0;
+        b = 0;
+        c = 0;
+    }
+
+    @Test
+    void the_triangle_should_be_scalene(){
+        a = 3;
+        b = 6;
+        c = 4;
+        assertEquals(3, triangle.triangle(a,b,c));
+    }
+    @Test
+    void the_triangle_should_be_isosceles(){
+        a = 3;
+        b = 6;
+        c = 3;
+        assertEquals(1, triangle.triangle(a,b,c));
+    }
+    @Test
+    void the_triangle_should_be_equilateral(){
+        a = 4;
+        b = 4;
+        c = 4;
+        assertEquals(2, triangle.triangle(a,b,c));
+    }
+    @Test
+    void the_triangle_should_not_be_a_triangle(){
+        a = -2;
+        b = 18;
+        c = 0;
+        assertEquals(4, triangle.triangle(a,b,c));
     }
 }
